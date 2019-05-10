55c309f2ac811d8e735c196be87f81c40949f7dc
Mateusz Mizio <mateusz1997-11@o2.pl>
2019-05-10 15:49:19 +0200

    Exercise 3

---
+++ 3/Script3.js
@@ -0,0 +1,30 @@
+function search() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("textField");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("searchTable");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function enterPressed() {
+    if (event.keyCode == 13) {
+        event.preventDefault();
+        search();
+    }
+}
+
+function buttonClicked() {
+    search();
+}
